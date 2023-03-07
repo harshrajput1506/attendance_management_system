27c4c43cf5dfa7e2ab146cecc765df239d3235e8
Avanish Raj Singh <avanishraj005@gmail.com>
2023-03-08 02:08:10 +0530

    added validator

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,21 +1,44 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'semesterpage.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
   State<StatefulWidget> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formkey = GlobalKey<FormState>();
-  final emailController = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  final empIdController = TextEditingController();
   final passwordController = TextEditingController();
+  final _streamController = StreamController<String>();
+
+  @override
+  void initState() {
+    super.initState();
+    empIdController.addListener(_checkInput);
+  }
 
   @override
   void dispose() {
-    emailController.dispose();
+    empIdController.dispose();
     passwordController.dispose();
     super.dispose();
+  }
+
+  void _checkInput() {
+    String value = empIdController.text.trim();
+    RegExp regExp = RegExp(r'[!@#<>?":_`~;[\]\\|=+)(*&^%$#@!]+');
+    if (regExp.hasMatch(value)) {
+      _streamController.add('Input must not contain special characters');
+    } else {
+      return;
+    }
   }
 
   @override
@@ -39,7 +62,6 @@ class _LoginScreenState extends State<LoginScreen> {
           // )
         ),
         body: Container(
-          key: _formkey,
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           child: SingleChildScrollView(
@@ -91,74 +113,93 @@ class _LoginScreenState extends State<LoginScreen> {
                             borderRadius: BorderRadius.circular(50),
                           ),
                           color: Color.fromRGBO(4, 29, 83, 1),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                margin: EdgeInsets.only(
-                                    left: 20, right: 20, top: 100),
-                                padding: EdgeInsets.only(left: 20, right: 20),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Color.fromRGBO(255, 255, 255, 1),
-                                ),
-                                alignment: Alignment.center,
-                                child: TextFormField(
+                          child: Form(
+                            key: _formkey,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.only(
+                                      left: 20, right: 20, top: 100),
+                                  padding: EdgeInsets.only(left: 20, right: 20),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Color.fromRGBO(255, 255, 255, 1),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: TextFormField(
                                     cursorColor: Colors.black,
                                     decoration: InputDecoration(
                                       icon: Icon(
                                         Icons.email,
                                       ),
-                                      hintText: "Enter Email",
+                                      hintText: "Enter ID",
                                       enabledBorder: InputBorder.none,
                                       focusedBorder: InputBorder.none,
                                     ),
-                                    controller: emailController,
-                                    validator: (String? value) {
-                                      if (value!.isEmpty) {
-                                        return "Please Enter email";
+                                    controller: empIdController,
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return "Enter your ID";
+                                      } else if (value != null) {
+                                        RegExp regExp = RegExp(
+                                            r'[!@#<>?":_`~;[\]\\|=+)(*&^%$#@!]+');
+                                        if (regExp.hasMatch(value)) {
+                                          return 'Input must not contain special characters';
+                                        }
+                                      } else {
+                                        return null;
                                       }
-
-                                      if (!RegExp(
-                                              "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                                          .hasMatch(value)) {
-                                        return "Please enter valid email";
-                                      }
-                                      return null;
-                                    }),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(
-                                    left: 20, right: 20, top: 30),
-                                padding: EdgeInsets.only(left: 20, right: 20),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Color.fromRGBO(255, 255, 255, 1),
+                                    },
+                                  ),
                                 ),
-                                alignment: Alignment.center,
-                                child: TextFormField(
-                                    keyboardType: TextInputType.emailAddress,
-                                    obscureText: true,
-                                    cursorColor: Colors.black,
-                                    decoration: InputDecoration(
-                                      icon: Icon(
-                                        Icons.vpn_key,
+                                SizedBox(height: 5,),
+                                StreamBuilder<String>(
+                                  stream: _streamController.stream,
+                                  builder: (context, snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Icon(
+                                        Icons.error,
+                                        color: Colors.red,
+                                      );
+                                    } else {
+                                      return Container();
+                                    }
+                                  },
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(
+                                      left: 20, right: 20, top: 30),
+                                  padding: EdgeInsets.only(left: 20, right: 20),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Color.fromRGBO(255, 255, 255, 1),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: TextFormField(
+                                      keyboardType: TextInputType.emailAddress,
+                                      obscureText: true,
+                                      cursorColor: Colors.black,
+                                      decoration: InputDecoration(
+                                        icon: Icon(
+                                          Icons.vpn_key,
+                                        ),
+                                        hintText: "Enter Password",
+                                        enabledBorder: InputBorder.none,
+                                        focusedBorder: InputBorder.none,
                                       ),
-                                      hintText: "Enter Password",
-                                      enabledBorder: InputBorder.none,
-                                      focusedBorder: InputBorder.none,
-                                    ),
-                                    controller: passwordController,
-                                    validator: (String? value) {
-                                      if (value!.isEmpty) {
-                                        return "Please Enter password";
-                                      }
-                                      return null;
-                                    }),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(
-                                    left: 20, right: 20, top: 30),
-                                child: ElevatedButton(
+                                      controller: passwordController,
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return "Please Enter password";
+                                        } else if (value.length < 4) {
+                                          return "atleast 4 characters";
+                                        }
+                                      }),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(
+                                      left: 20, right: 20, top: 30),
+                                  child: ElevatedButton(
                                     style: ElevatedButton.styleFrom(
                                       minimumSize: Size(327, 69),
                                       backgroundColor:
@@ -173,20 +214,18 @@ class _LoginScreenState extends State<LoginScreen> {
                                             fontFamily: "Poppins",
                                             fontWeight: FontWeight.w600)),
                                     onPressed: () {
-                                      if (_formkey.currentState != null) {
-                                        _formkey.currentState?.validate();
-                                        print("Successfully logged in");
-                                      } else {
-                                        print("Unsuccessful login");
+                                      if (_formkey.currentState!.validate()) {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    SemesterPage()));
                                       }
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  SemesterPage()));
-                                    }),
-                              ),
-                            ],
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
